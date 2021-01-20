e817de0e4c4e81e8cac0308dc7acc87d60834404
fandystar <jonasabok@gmail.com>
2021-01-20 20:19:38 +0700

    push 2

--- asses 1/src/Person.java
+++ asses 1/src/Person.java
@@ -1,14 +1,9 @@
 
 
-
-
-
-
-public class Person {
+public class Person  {
     String name;
     String address;
 
-    
     Person(String name,String address) {
         this.name=  name;
         this.address = address;
